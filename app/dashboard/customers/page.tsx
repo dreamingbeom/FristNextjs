f8e5f1a9687fdb78a3692579f3a84b9b20c9cfa9
김경범 <gimgyeongbeom@gimgyeongbeom-ui-MacBookAir.local>
2023-12-14 12:15:46 +0900

    .

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div>커스터머페이지</div>
+  )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div>인보이스 페이지</div>
+  )
+}
